24d1645f86de7d926b3c35f778b0adce1afd2bbe
amiramariastoica0-prog <amiramariastoica0@gmail.com>
2025-10-23 13:08:32 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>¡Hola, esta es una página web hecha con Python y Flask!</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
